8b3a4c5f90fb642aa361fe4151f097ab1e914479
Aaron de Mello <aaron.d@nylas.com>
2024-02-08 18:38:05 -0500

    Updated the index page to include a landing page. Also updated Tailwinds config to setup the new uprocket branding

---
+++ src/components/header.tsx
@@ -0,0 +1,84 @@
+import Link from 'next/link'
+import { JSX, SVGProps } from 'react'
+
+import { useLoggedInUser } from '@/lib/hooks'
+
+export function Header() {
+  const { user, isLoading } = useLoggedInUser()
+  return (
+    <header className='px-4 lg:px-6 h-14 flex items-center bg-apple-600'>
+      <Link className='flex items-center justify-center' href='/'>
+        <BriefcaseIcon className='h-6 w-6 text-white' />
+        <span className='sr-only'>UpRocket</span>
+      </Link>
+      <nav className='ml-auto flex gap-4 sm:gap-6'>
+        {/* <Link
+          className='text-sm font-medium text-white hover:underline underline-offset-4'
+          href='#'
+        >
+          Jobs
+        </Link> */}
+        <Link
+          className='text-sm font-medium text-white hover:underline underline-offset-4'
+          href='/contractors'
+        >
+          Contractors
+        </Link>
+        {/* <Link
+          className='text-sm font-medium text-white hover:underline underline-offset-4'
+          href='#'
+        >
+          Testimonials
+        </Link> */}
+        {!isLoading && (
+          <>
+            {user && (
+              <>
+                <Link
+                  className='text-sm font-medium text-white hover:underline underline-offset-4'
+                  href='/profile'
+                >
+                  Profile
+                </Link>
+                <Link
+                  className='text-sm font-medium text-white hover:underline underline-offset-4'
+                  href='/api/logout'
+                >
+                  Log Out
+                </Link>
+              </>
+            )}
+            {!user && (
+              <Link
+                className='text-sm font-medium text-white hover:underline underline-offset-4'
+                href='/login'
+              >
+                Log In
+              </Link>
+            )}
+          </>
+        )}
+      </nav>
+    </header>
+  )
+}
+
+function BriefcaseIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <rect width='20' height='14' x='2' y='7' rx='2' ry='2' />
+      <path d='M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16' />
+    </svg>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,55 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        apple: {
+          '50': '#ecffe5',
+          '100': '#d3ffc7',
+          '200': '#a9ff95',
+          '300': '#73fe58',
+          '400': '#46f526',
+          '500': '#23dc06',
+          '600': '#14a800',
+          '700': '#138506',
+          '800': '#14690b',
+          '900': '#14580f',
+          '950': '#033201',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [
+    require('tailwindcss-animate'),
+
+    function ({ addBase, theme }: { addBase: any; theme: any }) {
+      function extractColorVars(colorObj: { [x: string]: any }, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey]
+
+          const newVars: { [x: string]: any } =
+            typeof value === 'string'
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`)
+
+          return { ...vars, ...newVars }
+        }, {})
+      }
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      })
+    },
+  ],
+}
+export default config
