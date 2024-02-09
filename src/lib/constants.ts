cfd1bc4cd096704faf5894f89e674de079e64cd0
Aaron de Mello <aaron.d@nylas.com>
2024-02-09 14:26:27 -0500

    Fixed some typos and missing NYLAS_ prefixes in the env vars

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,9 +2,9 @@ import moment from "moment-timezone";
 export const NYLAS_AUTH_CONFIG = {
   storageType: "indexeddb",
   apiUri:
-    process.env.NEXT_PUBLIC_NYLAS_API_URI || "https://api-staging.nylas.com",
+    process.env.NEXT_PUBLIC_NYLAS_API_URL || "https://api-staging.nylas.com",
   elementsApiUri:
-    process.env.NEXT_PUBLIC_NYLAS_ELEMENTS_API_URI ||
+    process.env.NEXT_PUBLIC_NYLAS_ELEMENTS_API_URL ||
     "https://elements-staging.us.nylas.com",
   clientId: process.env.NEXT_PUBLIC_NYLAS_CLIENT_ID || "nylas-client-id",
   defaultScopes: ["https://www.googleapis.com/auth/calendar"],
